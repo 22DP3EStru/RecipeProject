9f782a1431d6194365f48d7b44f5612c72dce8c0
22DP3EStru <131261234+22DP3EStru@users.noreply.github.com>
2025-02-23 13:34:10 +0000

    .

--- sākums.php
+++ sākums.php
@@ -1,20 +1,21 @@
+<!-- filepath: /workspaces/RecipeProject/resources/views/sakums.blade.php -->
 <!DOCTYPE html>
 <html lang="lv">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modernā Recepšu Mājaslapa</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <header>
         <h1>Recepšu Mājaslapa</h1>
         <nav>
             <ul>
-                <li><a href="index.html">Sākums</a></li>
-                <li><a href="recipes.html">Receptes</a></li>
-                <li><a href="login.html">Pieslēgties</a></li>
-                <li><a href="register.html">Reģistrēties</a></li>
+                <li><a href="{{ url('/') }}">Sākums</a></li>
+                <li><a href="{{ url('/recipes') }}">Receptes</a></li>
+                <li><a href="{{ url('/login') }}">Pieslēgties</a></li>
+                <li><a href="{{ url('/register') }}">Reģistrēties</a></li>
             </ul>
         </nav>
     </header>
