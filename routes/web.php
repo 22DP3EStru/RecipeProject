532f482d6fa02a09b26ecd3f1ca78efc9c0fee1b
22DP3EStru <131261234+22DP3EStru@users.noreply.github.com>
2025-02-24 12:12:21 +0000

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", function () {
+    return view("home");
+})->name('homepage');
+
+Route::get("/MYlogin", function () {
+    return view("MYlogin");
+})->name('MYlogin');
+
+Route::get("/MYregister", function () {
+    return view("MYregister");
+})->name('MYregister');
+
+require DIR.'/auth.php';
